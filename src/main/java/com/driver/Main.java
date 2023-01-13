2360dc56246b0eb4fc297b961b0aeb773012a21a
viju-dev <71461702+viju-dev@users.noreply.github.com>
2023-01-14 00:44:47 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,20 @@
 package com.driver;
 
 public class Main {
-  public void main(String[] args) {
+  public static void main(String[] args) {
     B Obj1 = new B(); // showing error if make method static
     System.out.println(Obj1.meth());
     return;
 
   }
-  public class A{
+  public static class A{
     public String meth(){
       return "Invoking method from class A";
     }
 
   }
 
-  public class B extends A{
+  public static class B extends A{
     @Override
     public String meth(){
       return "Method is overridden in Extendend class B";
